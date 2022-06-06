23173a91196502d156723b25debd3786afe4eafc
chiuzon <89428507+chiuzon@users.noreply.github.com>
2022-06-06 13:53:59 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 
+// Change with your website url
+
+// export const SITE_URL =
+//   process.env.NODE_MODE === "development"
+//     ? "http://localhost:3000"
+//     : "https://chiuzon.github.io/prometheus-research-inc.github.io";
+// console.log(process.env);
+
 // https://astro.build/config
 export default /** @type {import('astro').AstroUserConfig} */ defineConfig({
   integrations: [svelte({})],
   buildOptions: {
-    site: "",
+    site: process.env.PUBLIC_SITE_URL,
     base: "prometheus-research-inc.github.io",
   },
 });
